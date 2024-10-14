dbadc269a04c8dd4b4234d79999ead8be3ea6214
Nonvignon <nonvignoncyrille3@gmail.com>
2024-10-14 17:25:52 -0400

    new files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('Healthy'); // Change to your response
+ });
+app.get('/', (req, res) => {
+	    res.send('Hello, World! This is my Node.js app running on ECS! This is my first app deployed on the ECS FARGATE, Now I will set up a jenkins pipeline to make this streamline and automate the process.');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+	    console.log(`Server is running on http://localhost:${PORT}`);
+});
